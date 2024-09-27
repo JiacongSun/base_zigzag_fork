e8a5cd91aacb6ee7ce25a4066f7f79bfcd0ebd9b
jiacong sun <jiacong.sun@kuleuven.be>
2024-09-27 23:03:18 +0200

    update x labels in plot

--- iscas/visualization.py
+++ iscas/visualization.py
@@ -1,8 +1,12 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.getcwd()))
+
 from zigzag.hardware.architecture.get_cacti_cost import get_cacti_cost
 import matplotlib.pyplot as plt
 import numpy as np
 import logging
-import os
 
 
 def plot_mem_comparison():
@@ -15,7 +19,7 @@ def plot_mem_comparison():
     ###############################
     # initialization
     info = {mem: {} for mem in ["dram", "sram", "bowen"]}
-    mem_size_list = [1024, 2048, 4096]
+    mem_size_list = [32 * 1024, 1024 * 1024, 32 * 1024 * 1024, 1024 * 1024 * 1024]
     for bw in [128]:
         for mem_size in mem_size_list:  # unit: B
             # initialize key with mem_size
@@ -32,7 +36,7 @@ def plot_mem_comparison():
             info["dram"][f"{mem_size}"]["r_cost_per_bit"] = r_cost_per_bit
             info["dram"][f"{mem_size}"]["w_cost_per_bit"] = w_cost_per_bit
             # extract sram result
-            access_time, area, r_cost, w_cost = get_cacti_cost(cacti_path="./zigzag/cacti/cacti_master",
+            access_time, area, r_cost, w_cost = get_cacti_cost(cacti_path="../zigzag/cacti/cacti_master",
                                                                tech_node=0.028,
                                                                mem_type="sram",
                                                                mem_size_in_byte=mem_size,
@@ -91,15 +95,24 @@ def plot_mem_comparison():
     # change x tick label
     for x in range(plot_num):
         axs[x].set_xticks(index)
-        axs[x].set_xticklabels([f"{x // 1024}" for x in mem_size_list])
+        labels = []
+        for mem_size in mem_size_list:
+            if mem_size < 1024*1024:
+                labels.append(f"{mem_size//1024}K")
+            elif mem_size < 1024*1024*1024:
+                labels.append(f"{mem_size // 1024 // 1024}M")
+            else:
+                labels.append(f"{mem_size//1024//1024//1024}G")
+        axs[x].set_xticklabels(labels)
     # add legend and grid
     for x in range(plot_num):
-        axs[x].legend(fontsize=12)
+        if x == 0:
+            axs[x].legend(fontsize=12)
         axs[x].grid(which="major", axis="y", color="gray", linestyle="--", linewidth=1)
         axs[x].set_axisbelow(True)
     # add label
     for x in range(plot_num):
-        axs[x].set_xlabel("Memory size [KB]", fontsize=14)
+        axs[x].set_xlabel("Memory size [B]", fontsize=14)
     axs[0].set_ylabel("Area [mm$^2$]", fontsize=14)
     axs[1].set_ylabel("Write energy [pJ/bit]", fontsize=14)
     axs[2].set_ylabel("Read energy [pJ/bit]", fontsize=14)
@@ -109,9 +122,10 @@ def plot_mem_comparison():
     for x in range(plot_num):
         axs[x].set_yscale("log")
     plt.tight_layout()
+    # plt.show()
     # save plt
-    os.makedirs("./iscas/", exist_ok=True)
-    plt.savefig("./iscas/mem_comparison.png", dpi=300, bbox_inches="tight")
+    # os.makedirs("./iscas/", exist_ok=True)
+    plt.savefig("./mem_comparison.png", dpi=300, bbox_inches="tight")
 
 
 if __name__ == "__main__":
@@ -121,5 +135,10 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging_level, format=logging_format)
     #########################################
     # Experiment 1: area, energy comparison among SRAM and Bowen's mem
+    # comment to bowen:
+    # (1) is it correct that E/bit does not scale with memory size for our case?
+    # (2) is the area trend and value make sense?
     plot_mem_comparison()
+    #########################################
+    # Experiment 2: zigzag evaluation result
     pass
